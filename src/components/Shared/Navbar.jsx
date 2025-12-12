61fb6d509a885dbb2ea9e0b1b0572d2ab12bbd4c
Shajidaa <shajidaakterlopa101@gmail.com>
2025-12-12 23:12:37 +0600

    u

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -155,7 +155,7 @@ function Navbar() {
                   <li onClick={closeMenu}>
                     <MyLink
                       className={({ isActive }) =>
-                        `px-4 py-2 rounded-md transition-all font-medium text-white ${
+                        `px-4 py-2 rounded-md transition-all font-medium text-orange-600 ${
                           isActive
                             ? "bg-(--color-secondary)"
                             : "hover:bg-(--color-secondary-hover)"
